018e59cfcbee7cb2fa8f145502911b6b95f3301b
bodnara-coder <67366730+bodnara-coder@users.noreply.github.com>
2025-11-21 16:21:41 +0100

    Add project_id and region variables to Terraform

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "project_id" {
+  description = "The GCP project ID to deploy resources into."
+  type        = string
+}
 
+variable "region" {
+  description = "The region for the VM instance (must be US-east1 for free tier eligibility)."
+  type        = string
+  default     = "us-east1"
+}
